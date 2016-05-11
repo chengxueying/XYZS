592198fe770f4d16be5ad4b674f6a1483f044979
yueying <2337412401@qq.com>
2016-05-11 19:24:01 +0800

    没做完

---
+++ app/src/main/java/xyzs/hy/com/xyzs/LiftActivity.java
@@ -0,0 +1,88 @@
+package xyzs.hy.com.xyzs;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.Button;
+
+import xyzs.hy.com.xyzs.common.HttpUtils;
+
+/**
+ * 生活查询页
+ */
+public class LiftActivity extends Activity implements View.OnClickListener {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_left);
+
+        Button btnMeiTuan = (Button) findViewById(R.id.btn_mei);
+        Button btnELM = (Button) findViewById(R.id.btn_e);
+        Button btnRuJia = (Button) findViewById(R.id.btn_ru);
+        Button btnQiTian = (Button) findViewById(R.id.btn_qi);
+        Button btnHuoChe = (Button) findViewById(R.id.btn_huo);
+        Button btnXieCheng = (Button) findViewById(R.id.btn_xie);
+        Button btnKuaiDi = (Button) findViewById(R.id.btn_kuai);
+        Button btnAiCha = (Button) findViewById(R.id.btn_ai);
+
+        btnMeiTuan.setOnClickListener(this);
+        btnELM.setOnClickListener(this);
+        btnRuJia.setOnClickListener(this);
+        btnQiTian.setOnClickListener(this);
+        btnHuoChe.setOnClickListener(this);
+        btnXieCheng.setOnClickListener(this);
+        btnKuaiDi.setOnClickListener(this);
+        btnAiCha.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_mei:
+                sendIntent(HttpUtils.MEI_TUAN);
+            break;
+            case R.id.btn_e:
+                sendIntent(HttpUtils.MEI_TUAN);
+
+                break;
+            case R.id.btn_ru:
+                sendIntent(HttpUtils.MEI_TUAN);
+                break;
+            case R.id.btn_qi:
+                sendIntent(HttpUtils.MEI_TUAN);
+                break;
+            case R.id.btn_huo:
+                sendIntent(HttpUtils.MEI_TUAN);
+                break;
+            case R.id.btn_xie:
+                sendIntent(HttpUtils.MEI_TUAN);
+                break;
+            case R.id.btn_kuai:
+                sendIntent(HttpUtils.MEI_TUAN);
+                break;
+            case R.id.btn_ai:
+                sendIntent(HttpUtils.MEI_TUAN);
+                break;
+
+
+        }
+
+
+    }
+    private void sendIntent(String name) {
+        Intent intent = new Intent(LiftActivity.this, QueryActivity.class);
+        intent.putExtra("name", name);
+
+        finish();
+    }
+}
---
+++ app/src/main/java/xyzs/hy/com/xyzs/common/HttpUtils.java
@@ -0,0 +1,31 @@
+package xyzs.hy.com.xyzs.common;
+
+/**
+ * Created by Administrator on 2016/5/11.
+ */
+public class HttpUtils {
+    //美团
+    public static String MEI_TUAN = "http://qd.meituan.com/?utm_campaign=baidu&utm_medium=organic&utm_source=baidu&utm_content=homepage&utm_term=";
+    //饿了么
+    public static String E_MA = "https://www.ele.me/home/http://cet.99sushe.com/";
+    //如家
+    public static String RU_JIA = "http://www.homeinns.com/";
+    //7天
+    public static String QI_TIAN = "http://www.7daysinn.cn/";
+    //火车票
+    public static String HUO_CHE_PIAO = "http://www.12306.cn/mormhweb/";
+    //携程
+    public static String XIE_CHENG = "http://www.ctrip.com/";
+    //快递
+    public static String KUAI_DI = "http://www.kuaidi100.com/";
+    //爱查
+    public static String AI_CHA = "http://www.ickd.cn/";
+
+    // 四六级查询网址
+    public static String FOUR_SCORE = "http://cet.99sushe.com/";
+
+    //成绩查询
+    public static String SCORE = "http://jw.qdu.edu.cn/academic/common/security/electiveLogin.jsp";
+
+
+}
